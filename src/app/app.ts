41273e73711f3901d53780f4c15d380b66528087
Jorge Kassabji <jorgekm21@gmail.com>
2025-11-26 18:14:15 -0400

    Commit 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Aside } from './components/aside/aside';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Aside, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
