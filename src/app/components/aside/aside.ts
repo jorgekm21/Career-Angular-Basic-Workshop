0dbdda0fc1b20e0533119f249d4c141cd0e8f10e
Jorge Kassabji <jorgekm21@gmail.com>
2025-11-26 21:35:34 -0400

    Fin clase data bindings

--- src/app/components/aside/aside.ts
+++ src/app/components/aside/aside.ts
@@ -7,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrl: './aside.css',
 })
 export class Aside {
+  activo = true
+  
 
+  filtrar = () : void => {
+    console.log("Borando elementos....")
+    const elementos = document.getElementsByClassName("mazda") as HTMLCollectionOf<HTMLElement>
+
+    for (let i = 0; i < elementos.length; i++){
+      elementos[i].style.display = 'none'
+    }
+
+    this.activo = false
+  }
+
+  resetear = () : void => {
+    console.log("Reseteando estilos....")
+    const elementos = document.getElementsByClassName("mazda") as HTMLCollectionOf<HTMLElement>
+    console.log(elementos)
+
+    const header = document.getElementById("header") as HTMLElement
+    const footer = document.getElementById("footer") as HTMLElement
+    const inputColor = document.getElementById("color") as HTMLInputElement
+
+    header.style.backgroundColor = "#ffe600"
+    footer.style.backgroundColor = "#ffe600"
+    inputColor.value = "#ffe600"
+
+    for (let i = 0; i < elementos.length; i++){
+      elementos[i].style.display = 'block'
+    }
+
+    this.activo = true
+  }
+
+  cambiarColor = () : void => {
+    const header = document.getElementById("header") as HTMLElement
+    const footer = document.getElementById("footer") as HTMLElement
+    const inputColor = document.getElementById("color") as HTMLInputElement
+
+    console.log("Colocando color: " + inputColor.value)
+    header.style.backgroundColor = inputColor.value
+    footer.style.backgroundColor = inputColor.value
+  }
 }
