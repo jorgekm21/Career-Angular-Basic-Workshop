c2ac4478bafcde6c2596fc3ab9533ba4c2831ab6
Jorge Kassabji <jorgekm21@gmail.com>
2025-11-28 20:42:51 -0400

    Ajustes con ngclass ngstyle ngif y ngfor

--- src/app/components/aside/aside.ts
+++ src/app/components/aside/aside.ts
@@ -1,28 +1,16 @@
 import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-aside',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './aside.html',
   styleUrl: './aside.css',
 })
 export class Aside {
   activo = true
-  
-
-  filtrar = () : void => {
-    const elementos = document.getElementsByClassName("mazda") as HTMLCollectionOf<HTMLElement>
-
-    for (let i = 0; i < elementos.length; i++){
-      elementos[i].style.display = 'none'
-    }
-
-    this.activo = false
-  }
 
   resetear = () : void => {
-    const elementos = document.getElementsByClassName("mazda") as HTMLCollectionOf<HTMLElement>
-
     const header = document.getElementById("header") as HTMLElement
     const footer = document.getElementById("footer") as HTMLElement
     const menu_aside = document.getElementById("menu_aside") as HTMLElement
@@ -32,10 +20,6 @@ export class Aside {
     footer.style.backgroundColor = "#ffe600"
     menu_aside.style.backgroundColor = "#ffe600"
     inputColor.value = "#ffe600"
-
-    for (let i = 0; i < elementos.length; i++){
-      elementos[i].style.display = 'block'
-    }
 
     this.activo = true
   }
@@ -50,5 +34,7 @@ export class Aside {
     header.style.backgroundColor = inputColor.value
     footer.style.backgroundColor = inputColor.value
     menu_aside.style.backgroundColor = inputColor.value
+
+    this.activo = false
   }
 }
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+//SE HIZO CON CHATGPT
+interface Producto {
+  titulo: string;
+  descripcion: string;
+  precio: number;
+  ubicacion: string; 
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
 
+  productos: Producto[] = []
+  precioTotal = 0
+
+  agregarProducto = () : void => {
+    const tituloProducto = document.getElementById("titulo") as HTMLInputElement
+    const descripcionProducto = document.getElementById("descripcion") as HTMLInputElement
+    const precioProducto = document.getElementById("precio") as HTMLInputElement
+    const ubicacionProducto = document.getElementById("ubicacion") as HTMLInputElement
+
+    if (tituloProducto.value == "" || tituloProducto.value == null){
+      alert("Introduce el titulo del producto")
+      return
+    }
+
+    if (descripcionProducto.value == "" || descripcionProducto.value == null){
+      alert("Introduce el descripcion del producto")
+      return
+    }
+
+    const precioNumerico = parseInt(precioProducto.value)
+    if (precioNumerico <= 0 || precioNumerico == null){
+      alert("Introduce el precio del producto")
+      return
+    }
+
+    if (ubicacionProducto.value == "" || ubicacionProducto.value == null){
+      alert("Introduce el ubicacion del producto")
+      return
+    }
+
+    this.productos.push({
+      "titulo": tituloProducto.value,
+      "descripcion": descripcionProducto.value,
+      "precio": precioNumerico,
+      "ubicacion": ubicacionProducto.value
+    })
+
+    this.precioTotal += precioNumerico
+  }
 }
